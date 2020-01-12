8815bd9ba0bd2ed0bfc2ddb7e5d93a91bf83ff09
My Siira <my_siira@hotmail.com>
2020-01-12 18:20:56 +0100

    changed the structure and appearance of the page, and added some error handling through JS

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-const inputDate = document.querySelector('input#start');
+const buttonChooseByName = document.querySelector('#button-choose-by-name');
 
-//const inputName = document.querySelector('input#name');
-
-//const date = document.querySelector('#start').value;
-
-const button = document.querySelector('button');
-
-//const name = document.querySelector('#name').value;
-
-const country = document.querySelector('option');
-
-
+const buttonChooseByDate = document.querySelector('#button-choose-by-date');
 
 const form = document.querySelector('form');
 
 const output = document.querySelector('#output');
 
-/* let myObject = {
-    se: "My, Ch"
-};
+//Handle Search By Name-button, hide buttons and display possibility to search by name
+buttonChooseByName.addEventListener('click', () => {
 
-console.log(Object.values(myObject)); */
+    document.querySelector('#search-by-name').classList.remove('d-none');
 
-//
+    document.querySelector('#buttons').classList.add('d-none');
+});
+
+//Handle Search By Date-button, hide buttons and display possibility to search by date
+buttonChooseByDate.addEventListener('click', () => {
+
+    document.querySelector('#search-by-date').classList.remove('d-none');
+
+    document.querySelector('#buttons').classList.add('d-none');
+});
+
+//Handle Back-button, so the user can go back to the beginning
+form.addEventListener('click', (e) => {
+
+    //console.log(e.target.textContent);
+    if(e.target.textContent === 'Back'){
+        document.querySelector('#search-by-date').classList.add('d-none');
+        document.querySelector('#search-by-name').classList.add('d-none');
+        document.querySelector('#buttons').classList.remove('d-none');
+        output.innerHTML = '';
+    }
+});
+
+//When user clicks Search, check which 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -32,41 +44,113 @@ form.addEventListener('submit', (e) => {
 
     const inputDay = document.querySelector('#days').value;
 
-    const countryInput = document.querySelector('select#country').value;
+    const inputCountry = document.querySelector('select#country').value;
+
+
 
     if(inputName){
         console.log(inputName);
 
-        getNameDayByName(inputName, countryInput);
+        getNameDayByName(inputName, inputCountry);
 
         document.querySelector('#name').value = "";
     } else if(inputMonth && inputDay){
 
-        getNameDayByDate(inputMonth, inputDay, countryInput);
+        getNameDayByDate(inputMonth, inputDay, inputCountry);
+
+        //console.log('classlist of day is:', document.querySelector('#days').classList);
+
+        //Ta bort felindikering - 
+        if(document.querySelector('#days').classList.contains('border-danger')){
+
+            console.log('days has the class border-danger');
+            document.querySelector('#days').classList.remove('border-danger');
+        } else if(document.querySelector('#months').classList.contains('border-danger')){
+
+            console.log('days has the class border-danger');
+            document.querySelector('#days').classList.remove('border-danger');
+        }
+        
+        //Clear the date fields for new search
+        document.querySelector('#months').value = "";
+        document.querySelector('#days').value = "";
+
+
+    } else if(inputMonth && !inputDay){
+        output.innerHTML = `
+        <div class="output-wrapper card-body alert alert-light card col-md-6 mx-auto" role="alert">
+            <p class="card-title">You have to submit month and day.</p>
+        </div>
+        `;
+        document.querySelector('#days').classList.add('border-danger');
+       
+    } else if(!inputMonth && inputDay){
+        output.innerHTML = `
+        <div class="output-wrapper card-body alert alert-light card col-md-6 mx-auto" role="alert">
+            <p class="card-title">You have to submit month and day.</p>
+        </div>
+        `;
+        document.querySelector('#months').classList.add('border-danger');
+    } else if(!inputMonth && !inputDay){
+        output.innerHTML = `
+        <div class="output-wrapper card-body alert alert-light card col-md-6 mx-auto" role="alert">
+            <p class="card-title">You have to submit month and day.</p>
+        </div>
+        `;
+        document.querySelector('#months').classList.add('border-danger');
+        document.querySelector('#days').classList.add('border-danger');
     }
 });
 
+//Remove border if user clicks on date or month
+form.addEventListener('click', (e) => {
+    console.log(e.target.id);
+    if(e.target.id === 'months'){
+        document.querySelector('#months').classList.remove('border-danger');
+    } else if(e.target.id === 'days'){
+        document.querySelector('#days').classList.remove('border-danger');
+    }
+});
+
+
 //Show result of search by date in the DOM 
-const renderDateResult = (names, month, day, country) => {
-
-
-    console.log('country is:', country);
-    console.log(names);
-
-    console.log(names[0].namedays);
+const renderDateResult = (names, month, day) => {
 
     const html =`
     <div class="output-wrapper card col-md-6 mx-auto">
         <div class="card-body">
             <p class="card-title">The following has name day ${day}/${month}:</p>
-            <ul id="name-list">
-                
-            </ul>
+            <ul id="name-list"></ul>
         </div>
     </div>
     `;
 
     output.innerHTML = html;
+
+    calcNamesByDate(names);
+
+
+/*     const list = document.querySelector('#name-list');
+
+    const listItem = document.createElement('li');
+
+    names.forEach((name) => {
+
+        const nameArray = Object.values(name);
+
+        const nameByCountry = Object.values(nameArray[1]);
+
+        console.log(nameByCountry);
+
+        listItem.innerText = nameByCountry[0];
+        list.append(listItem);
+    }); */
+
+}
+
+const calcNamesByDate = (names) => {
+
+    console.log('in the calc names function');
 
     const list = document.querySelector('#name-list');
 
@@ -82,54 +166,33 @@ const renderDateResult = (names, month, day, country) => {
 
         listItem.innerText = nameByCountry[0];
         list.append(listItem);
+
+        
     });
 
-}
+};
 
 //Show result of search by name in the DOM 
 //ADD THE NAMES OF THE OTHERS WHO ALSO HAS NAME DAY
 const renderNameResult = (inputName, month, day) => {
 
+    console.log('in the rendernameresult function before html');
+
     const html =`
     <div class="output-wrapper card col-md-6 mx-auto">
         <div class="card-body">
             <h2 class="card-title">${inputName} has name day ${day}/${month}.</h2>
-            <p class="card-text">Others that have name day the same day are: ${names}</p>
+            <p class="card-text">Others that have name day the same day are:</p>
+            <ul id="name-list"></ul>
         </div>
     </div>
     `;
 
+    console.log('in the rendernameresult function');
+
     output.innerHTML = html;
 }
 
-//Adding event to our search button
-/* button.addEventListener('click', (e) => {
-
-    //Prevent the submit event, i.e. prevent page from reloading
-    e.preventDefault();
-    
-    //Getting the date from user
-    const date = document.querySelector('#start').value;
-
-    //Getting the name from user
-    const name = document.querySelector('#name').value;
-
-    //console.log(`Name is: ${name.value} and country is: ${country.value}`);
-
-    if(name && country){
-        console.log(`Name is: ${name} and country is: ${country.selectedIndex}`);
-        
-    } else if(date){
-        console.log(date);
-        
-    }
-
-}); */
-
-/* countryInput.addEventListener('click', (e) => {
-    console.log(e);
-    console.log(e.target.attributes.value);
-}); */
 
 
 
@@ -155,6 +218,23 @@ const renderNameResult = (inputName, month, day) => {
 
 //Disable the name field when date is filled
 //inputName.setAttribute('disabled', '');
+
+/* //Disables the date search when the user uses name search
+form.name.addEventListener('keyup', (e) => {
+
+    const month = document.querySelector('#months');
+
+    const day = document.querySelector('#days');
+
+    if(e.target.value){
+        month.setAttribute('disabled', '');
+        day.setAttribute('disabled', '');
+    } else{
+        month.removeAttribute('disabled');
+        day.removeAttribute('disabled');
+    }
+
+}); */
 
 
 //Supported countrys 
--- nameday.js
+++ nameday.js
@@ -1,5 +1,67 @@
 const BASE_URL = 'https://api.abalin.net/';
 
+//Fetch API by name and country
+const getNameDayByName = (inputName, inputCountry) => {
+
+    const QUERY = `?name=${inputName}`;
+
+    fetch(`${BASE_URL}getdate${QUERY}&country=${inputCountry}`).then((response) => {
+        return response.json();
+    }).then(data => {
+
+        //Go through all the results that match the search and choose only the ones that is a perfect match
+        const filteredResults = data.results.filter((object) =>{
+
+            if(object.name.toLowerCase() === inputName){
+                return true;
+            } 
+        });
+
+        //Calling the function that output the results in the DOM
+        renderNameResult(filteredResults[0].name, filteredResults[0].month, filteredResults[0].day);
+
+        //getNameDayByDate(filteredResults[0].month, filteredResults[0].day, inputCountry)
+
+    }).catch(err => { 
+        //Let user know that the name does not exist as name day
+        output.innerHTML = `
+        <div class="output-wrapper card-body alert alert-light card col-md-6 mx-auto" role="alert">
+            <p class="card-title">The name does not have a name day. Try another.</p>
+        </div>
+        `;
+    });
+};
+
+//Fetch API by date and country
+const getNameDayByDate = (month, day, inputCountry) => {
+
+    const QUERY = `?country=${inputCountry}&month=${month}&day=${day}`;
+
+    fetch(`${BASE_URL}namedays${QUERY}`).then((response) => {
+        return response.json();
+    }).then(data => {
+
+        //Calling the function that output the results in the DOM
+        renderDateResult(data.data, month, day);
+
+    }).catch(err => { 
+
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Exempel
@@ -12,69 +74,3 @@ const byDay = 'https://api.abalin.net/namedays?country=us&month=7&day=15';
 
 //Parametrar namn och land
 const byName = 'https://api.abalin.net/getdate?name=John&country=us';
-
-//Fetch API by name and country set to Sweden
-const getNameDayByName = (inputName, inputCountry) => {
-
-    const QUERY = `?name=${inputName}`;
-
-    fetch(`${BASE_URL}getdate${QUERY}&country=${inputCountry}`).then((response) => {
-        return response.json();
-    }).then(data => {
-
-        const filteredResults = data.results.filter((object) =>{
-
-            if(object.name.toLowerCase() === inputName){
-                return true;
-            }
-        });
-
-        renderNameResult(filteredResults[0].name, filteredResults[0].month, filteredResults[0].day);
-    }).catch(err => { 
-
-    });
-};
-
-//Fetch API by date and country set to Sweden
-const getNameDayByDate = (month, day, inputCountry) => {
-
-    const QUERY = `?country=${inputCountry}&month=${month}&day=${day}`;
-
-    fetch(`${BASE_URL}namedays${QUERY}`).then((response) => {
-        return response.json();
-    }).then(data => {
-
-        //console.log(data.data.namedays.);
-
-        renderDateResult(data.data, month, day, inputCountry);
-        //console.log('Month is:', inputMonth);
-        //console.log('Day is:', inputDay);
-
-/*         const filteredResults = data.results.filter((object) =>{
-
-            if(object.name.toLowerCase() === inputName){
-                return true;
-            }
-        });
-
-        renderNameResult(filteredResults[0].name, filteredResults[0].month, filteredResults[0].day); */
-    }).catch(err => { 
-
-    });
-};
-
-/* //Fetch API by name and country
-const getNameDayByName = (name, country) => {
-
-    const QUERY = `?name=${name.value}&country=${country}`;
-
-    fetch(`${BASE_URL}getdate${QUERY}`).then((response) => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
-    }).catch(err => {
-
-    });
-};
- */
-
